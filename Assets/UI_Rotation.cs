10a745549b433b95bccbac030e1203e5dce46992
Vilhelm Stokstad <v.stokstad@gmail.com>
2020-08-21 23:50:02 +0200

    Updates to UI and EnemyShip

---
+++ Assets/UI_Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Rotation : MonoBehaviour
+{
+    public GameObject _body;
+    
+
+   
+
+    
+    void FixedUpdate()
+    {
+
+//        _transformRotation.y 
+        _body.transform.Rotate(xAngle: 0f, yAngle: 10f, zAngle: 0f);
+    }
+}
